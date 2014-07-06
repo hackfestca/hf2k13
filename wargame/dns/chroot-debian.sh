b22ee6babafe50a14aa2b94b60be4dab87d47154
Guillaume Parent <gparent@gparent.org>
2014-07-06 13:12:17 +0000

    /var/run/bind -> /var/run/named

--- wargame/dns/chroot-debian.sh
+++ wargame/dns/chroot-debian.sh
@@ -20,10 +20,10 @@ mknod $CHROOT/dev/null c 1 3
 mknod $CHROOT/dev/random c 1 8
 chmod 666 $CHROOT/dev/null
 chmod 666 $CHROOT/dev/random
-mkdir -p $CHROOT/var/cache $CHROOT/var/log $CHROOT/var/run $CHROOT/var/log/named $CHROOT/var/run/bind $CHROOT/var/cache/bind
+mkdir -p $CHROOT/var/cache $CHROOT/var/log $CHROOT/var/run $CHROOT/var/log/named $CHROOT/var/run/named $CHROOT/var/cache/bind
 chmod 770 $CHROOT/var/log/named
-chmod 775 $CHROOT/var/run/bind $CHROOT/var/cache/bind
-chown root:bind $CHROOT/var/log/named $CHROOT/var/run/bind $CHROOT/var/cache/bind
+chmod 775 $CHROOT/var/run/named $CHROOT/var/cache/bind
+chown root:bind $CHROOT/var/log/named $CHROOT/var/run/named $CHROOT/var/cache/bind
 sed -e "s,\"-u bind\",\"-t /chroot/bind -u bind\"," /etc/default/bind9 > /tmp/x
 mv /tmp/x /etc/default/bind9
 
